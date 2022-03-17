722bf7c41a41f6e957f2439d094d6ca8a9a11137
Melkfree <yevhen.ihnatiuk@gmail.com>
2022-03-17 14:35:37 +0200

    filter done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters:[]
+      monsters:[],
+      searchField: ''
     }
   }
 
@@ -20,9 +21,20 @@ class App extends Component {
   }
 
   render(){
+
+    const filteredMonsters = this.state.monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input className='search-box' type="search" placeholder='Seacrh monsters' onChange={
+          (event)=>{
+            console.log(event.target.value);
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(()=>{return {searchField}})
+          }}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
